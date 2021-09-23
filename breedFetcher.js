f8144cbdccebb026f84c3532ea18968ed255f0b9
Russell Engebretson <russell.engebretson@gmail.com>
2021-09-23 20:17:56 +0000

    first commit

---
+++ breedFetcher.js
@@ -0,0 +1,17 @@
+const request = require('request');
+const args = process.argv.slice(2);
+
+request("https://api.thecatapi.com/v1/breeds/search?q=" + args[0], (error, response, body) => {
+  if (error) {
+    console.log("This URL domain does not seem to exist");
+  } else if (response.statusCode === 404) {
+    console.log("No such URL endpoint exists");
+  } else {
+    const data = JSON.parse(body);
+    if (data.length < 1) {
+      console.log('Breed not found')
+    } else {
+      console.log(data[0].description);
+    }
+  }
+});
